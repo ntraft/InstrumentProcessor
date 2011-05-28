5b2e4e1a3544fc634a7dc4e246b5f3697878b541
Neil Traft <ntraft@gmail.com>
2011-05-28 09:49:43 -0400

    Before using Mockito.

--- src/org/bostoncodingdojo/DefaultInstrumentProcessor.java
+++ src/org/bostoncodingdojo/DefaultInstrumentProcessor.java
@@ -1,16 +1,31 @@
 package org.bostoncodingdojo;
 
+
 public class DefaultInstrumentProcessor implements InstrumentProcessor {
 	
 	private final TaskDispatcher dispatcher;
+	private final Instrument instrument;
 
-	DefaultInstrumentProcessor(TaskDispatcher dispatcher) {
+	DefaultInstrumentProcessor(TaskDispatcher dispatcher, Instrument instrument) {
 		this.dispatcher = dispatcher;
+		this.instrument = instrument;
+		instrument.addInstrumentListener(new InstrumentListener() {
+			@Override
+			public void taskFinished(String task) {
+				DefaultInstrumentProcessor.this.dispatcher.finishedTask(task);
+			}
+			
+			@Override
+			public void taskError(String task) {
+				System.err.println("Error executing task: " + task);
+			}
+		});
 	}
 
 	@Override
 	public void process() {
-		dispatcher.finishedTask(dispatcher.getTask());
+		String task = dispatcher.getTask();
+		instrument.execute(task);
 	}
 	
 }
--- srcTest/org/bostoncodingdojo/InstrumentProcessorTest.java
+++ srcTest/org/bostoncodingdojo/InstrumentProcessorTest.java
@@ -1,26 +1,39 @@
 package org.bostoncodingdojo;
 
+import static org.junit.Assert.fail;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class InstrumentProcessorTest {
-	
+
 	private TaskDispatcher dispatcher;
 	private InstrumentProcessor processor;
 	private String[] tasks;
+	private Instrument instrument;
+	private TestInstrumentListener instrumentListener;
 
 	@Before
 	public void before() {
 		tasks = new String[]{"jocular", "infinite", "drastic", "popsicle", "comeback"};
+		// TODO Don't use default implementations, instead use mocks.
 		dispatcher = new DefaultTaskDispatcher(tasks);
-		processor = new DefaultInstrumentProcessor(dispatcher);
+		instrument = new DefaultInstrument();
+		instrumentListener = new TestInstrumentListener();
+		instrument.addInstrumentListener(instrumentListener);
+		// Problem: I have to pass in the objects I want it to delegate to, so I can
+		// confirm that they were actually used. Would I still have this problem if I
+		// were using a mocking framework?
+		processor = new DefaultInstrumentProcessor(dispatcher, instrument);
 	}
 	
 	@After
 	public void after() {
 		tasks = null;
 		dispatcher = null;
+		instrument = null;
+		instrumentListener = null;
 		processor = null;
 	}
 	
@@ -28,7 +41,47 @@ public class InstrumentProcessorTest {
 	public void testProcessExecutesNextTask() throws Exception {
 		for (String task : tasks) {
 			processor.process();
-			// TODO
+			// TODO assert Instrument.execute() is called with task.
+		}
+	}
+	
+	@Test
+	public void testProcessFinishesNextTask() throws Exception {
+		for (String task : tasks) {
+			processor.process();
+			// TODO assert TaskDispatcher.finishedTask() is called with task.
+		}
+	}
+	
+	@Test
+	public void testExceptionsBubbleUpToCaller() throws Exception {
+		// TODO make Instrument.execute() throw exceptions
+		try {
+			processor.process();
+			fail();
+		} catch (Throwable t) {
+			// Success.
+		}
+	}
+	
+	@Test
+	public void testErrorsAreWrittenToLog() throws Exception {
+		// TODO
+	}
+	
+	private final class TestInstrumentListener implements InstrumentListener {
+		
+		String lastExecutedTask;
+		String lastErrorTask;
+		
+		@Override
+		public void taskFinished(String task) {
+			lastExecutedTask = task;
+		}
+		
+		@Override
+		public void taskError(String task) {
+			lastErrorTask = task;
 		}
 	}
 }
