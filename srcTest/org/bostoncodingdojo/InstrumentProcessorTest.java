d04e55ce05f2876c602727a2e1f0f2a5f6b8566f
Neil Traft <ntraft@gmail.com>
2011-05-29 10:22:58 -0400

    Changing master branch into a skeleton. This will be the starting point for the kata.

--- srcTest/org/bostoncodingdojo/InstrumentProcessorTest.java
+++ srcTest/org/bostoncodingdojo/InstrumentProcessorTest.java
@@ -1,113 +1,26 @@
 package org.bostoncodingdojo;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class InstrumentProcessorTest {
 
-	private TestTaskDispatcher dispatcher;
-	private InstrumentProcessor processor;
-	private String[] tasks;
-	private DefaultInstrument instrument;
-	private TestInstrumentListener instrumentListener;
-
-	@Before
-	public void before() {
-		tasks = new String[]{"jocular", "infinite", "drastic", "popsicle", "comeback"};
-		dispatcher = new TestTaskDispatcher(tasks);
-		instrument = new DefaultInstrument();
-		instrumentListener = new TestInstrumentListener();
-		instrument.addInstrumentListener(instrumentListener);
-		// Problem: I have to pass in the objects I want it to delegate to, so I can
-		// confirm that they were actually used. Would I still have this problem if I
-		// were using a mocking framework?
-		processor = new DefaultInstrumentProcessor(dispatcher, instrument);
-	}
-	
-	@After
-	public void after() {
-		tasks = null;
-		dispatcher = null;
-		instrument = null;
-		instrumentListener = null;
-		processor = null;
-	}
-	
 	@Test
 	public void testProcessExecutesNextTask() throws Exception {
-		for (String task : tasks) {
-			processor.process();
-			Thread.sleep(500);
-			assertEquals(task, instrumentListener.lastExecutedTask);
-		}
+		// TODO
 	}
 	
 	@Test
 	public void testProcessFinishesNextTask() throws Exception {
-		for (String task : tasks) {
-			processor.process();
-			assertEquals(task, dispatcher.lastFinishedTask);
-		}
+		// TODO
 	}
 	
 	@Test
 	public void testExceptionsBubbleUpToCaller() throws Exception {
-		instrument = new ExceptionThrowingInstrument();
-		processor = new DefaultInstrumentProcessor(dispatcher, instrument);
-		try {
-			processor.process();
-			fail();
-		} catch (Throwable t) {
-			// Success.
-		}
+		// TODO
 	}
 	
 	@Test
 	public void testErrorsAreWrittenToLog() throws Exception {
-		// If we really wanted to test this, we would need the ability to pass a custom
-		// PrintStream into the processor, so we could see what its output is. But I'm
-		// not going to do that because we can already see that's overkill.
-	}
-
-	private final class TestTaskDispatcher extends DefaultTaskDispatcher {
-		
-		private String lastFinishedTask;
-
-		TestTaskDispatcher(String[] tasks) {
-			super(tasks);
-		}
-		
-		@Override
-		public void finishedTask(String task) {
-			super.finishedTask(task);
-			lastFinishedTask = task;
-		}
-	}
-	
-	private final class ExceptionThrowingInstrument extends DefaultInstrument {
-		@Override
-		public void execute(String task) {
-			throw new RuntimeException("Surprise!");
-		}
-	}
-	
-	private final class TestInstrumentListener implements InstrumentListener {
-		
-		String lastExecutedTask;
-		String lastErrorTask;
-		
-		@Override
-		public void taskFinished(String task) {
-			lastExecutedTask = task;
-		}
-		
-		@Override
-		public void taskError(String task) {
-			lastErrorTask = task;
-		}
+		// TODO
 	}
 }
